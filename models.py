a823a605d906f8eb4e6676e89f28322c76cfc069
root <root@LAPTOP-A2EG90UI.localdomain>
2020-09-24 11:15:56 +0800

    update

--- models.py
+++ models.py
@@ -28,6 +28,26 @@ def MLP(user_num, item_num, latent_dim = 10, units = [20, 10]):
   results = tf.keras.layers.Dense(units = 1, activation = tf.math.sigmoid, kernel_regularizer = tf.keras.regularizers.L2(), bias_regularizer = tf.keras.regularizers.L2())(results);
   return tf.keras.Model(inputs = (users, items), outputs = results);
 
+def Fusion(user_num, item_num, latent_dim = 10, units = [20, 10]):
+
+  users = tf.keras.Input((1,), dtype = tf.int32); # users.shape = (batch, 1)
+  items = tf.keras.Input((1,), dtype = tf.int32); # items.shape = (batch, 1)
+  users_mf_embed = tf.keras.layers.Embedding(user_num, latent_dim, embeddings_regularizer = tf.keras.regularizers.L2())(users); # users_mf_embed.shape = (batch, 1, latent_dim)
+  users_mlp_embed = tf.keras.layers.Embedding(user_num, latent_dim, embeddings_regularizer = tf.keras.regularizers.L2())(users); # users_mlp_embed.shape = (batch, 1, latent_dim)
+  items_mf_embed = tf.keras.layers.Embedding(item_num, latent_dim, embeddings_regularizer = tf.keras.regularizers.L2())(items); # items_mf_embed.shape = (batch, 1, latent_dim)
+  items_mlp_embed = tf.keras.layers.Embedding(item_num, latent_dim, embeddings_regularizer = tf.keras.regularizers.L2())(items); # items_mlp_embed.shape = (batch, 1, latent_dim)
+  users_mf_embed = tf.keras.layers.Flatten()(users_mf_embed);
+  items_mf_embed = tf.keras.layers.Flatten()(items_mf_embed);
+  mf_results = tf.keras.layers.Lambda(lambda x: x[0] * x[1])([users_mf_embed, items_mf_embed]);
+  users_mlp_embed = tf.keras.layers.Flatten()(users_mlp_embed);
+  items_mlp_embed = tf.keras.layers.Flatten()(items_mlp_embed);
+  mlp_results = tf.keras.layers.Concatenate(axis = -1)([users_mlp_embed, items_mlp_embed]);
+  for layer_units in units:
+    mlp_results = tf.keras.layers.Dense(units = layer_units, kernel_regularizer = tf.keras.regularizers.L2(), bias_regularizer = tf.keras.regularizers.L2())(mlp_results);
+  results = tf.keras.layers.Concatenate(axis = -1)([mf_results, mlp_results]);
+  results = tf.keras.layers.Dense(units = 1, activation = tf.math.sigmoid, kernel_regularizer = tf.keras.regularizers.L2(), bias_regularizer = tf.keras.regularizers.L2())(results);
+  return tf.keras.Model(inputs = (users, items), outputs = results);
+
 if __name__ == "__main__":
 
   assert tf.executing_eagerly();
@@ -35,5 +55,8 @@ if __name__ == "__main__":
   gmf.save('gmf.h5');
   mlp = MLP(10,100);
   mlp.save('mlp.h5');
+  fusion = Fusion(10,100);
+  fusion.save('fusion.h5');
   tf.keras.utils.plot_model(model = gmf, to_file = 'GMF.png', show_shapes = True, dpi = 64);
   tf.keras.utils.plot_model(model = mlp, to_file = 'MLP.png', show_shapes = True, dpi = 64);
+  tf.keras.utils.plot_model(model = fusion, to_file = 'Fusion.png', show_shapes = True, dpi = 64);
